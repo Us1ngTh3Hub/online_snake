6b2d9bac54d725af0a451209e1e9f330d5ad584d
lok2 <pascal@fitzner.at>
2023-05-30 16:46:32 +0200

    added send and receive function

--- src/eudpylib/eudpylib/eudpylib.py
+++ src/eudpylib/eudpylib/eudpylib.py
@@ -2,38 +2,50 @@ import socket
 import checksum
 import headercreator
 
+# basic functions
+def split_into_fixed_parts(data, part_size):
+    num_parts = (len(data) + part_size - 1) // part_size
+    return [data[i*part_size:(i+1)*part_size] for i in range(num_parts)]
+
 # base class for client and server shared functions
 class eudpyBase:
-    def __init__(self, _ip, _port):
-        self.ip = _ip
-        self.port = _port
+    def __init__(self):
+        self.ip = socket.gethostbyname(socket.gethostname())
+        self.port = 54321
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
 
     def send(self, data):
         # encode message
         encoded_data = data.encode('utf-8')
-        # generate header
-        
         # generate checksum
-
+        data_checksum = checksum.calculate_checksum(data)
+        # generate header
+        header = headercreator.create_udp_header(self.port, self.port, len(data), data_checksum)
         # construct data
-
+        send_data = encoded_data + header
         # split data if it is too big for single packet
-
-        # transmit data
-        return
+        if(len(send_data)>1024):
+            for part in split_into_fixed_parts(send_data,1024):
+                self.sock.sendto(part, self.server_address)
+        else:
+            # transmit data
+            self.sock.sendto(send_data, self.server_address)
 
     def receive(self, buffer):
         # receive message
-
+        data, address = self.sock.recvfrom(1024)
+        header = headercreator.unpack_header(data[:8])
+        message = data[8:]
         # check header
-
-        # join data if it was split before
-
-        # review checksum
-
+        while header[2] != len(message):
+            # join data if it was split before
+            data, address = self.sock.recvfrom(1024)
+            # review checksum
+            if header[3] != checksum.calculate_checksum(data):
+                return -1
+            message += data
         # decrypt data
-        return
+        return message.decode('utf-8')
 
 # class for client specific functions
 class eudpyClient(eudpyBase):
