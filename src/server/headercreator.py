cd8047e452903dff676f200c49dd1e04e24082df
Luca Tiedemann <s200398@student.dhbw-mannheim.de>
2023-05-31 18:36:29 +0200

    Added Changes from branch to main see branch for detailed commits

---
+++ src/server/headercreator.py
@@ -0,0 +1,29 @@
+import struct
+
+header_format = "!HHHH"  # Format string for the header fields
+
+def unpack_header(header_data):
+    header = struct.unpack(header_format, header_data)
+    return header
+
+def create_udp_header(source_port, destination_port, length, checksum):
+    # UDP header structure: 2 bytes for source port, 2 bytes for destination port,
+    # 2 bytes for length, and 2 bytes for checksum
+    udp_header = struct.pack(header_format, source_port, destination_port, length, checksum)
+    return udp_header
+
+def calculate_checksum(message):
+    checksum = 0
+
+    for i in range(0, len(message), 2):
+        # Get the current 16-bit chunk from the message
+        chunk = (message[i] << 8) + message[i + 1]
+        checksum += chunk
+
+        # Wrap around if the sum exceeds 16 bits
+        checksum = (checksum & 0xFFFF) + (checksum >> 16)
+
+    # One's complement the checksum
+    checksum = ~checksum & 0xFFFF
+
+    return checksum
---
+++ src/server/server.py
@@ -0,0 +1,74 @@
+import socket
+import headercreator
+import threading
+import game
+
+# Colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+color = [blue, green]
+
+def send(socket, server_address, _message):
+    encoded_message = _message.encode('utf-8')
+    header = getheader(encoded_message)
+    complete_message = header + encoded_message
+    if(len(complete_message)>1024):
+        for part in split_into_fixed_parts(complete_message,1024):
+            socket.sendto(part, server_address)
+    else:
+        socket.sendto(complete_message, server_address)
+
+def getheader(self, encoded_message):
+    len = len(encoded_message)
+    summ = headercreator.calculate_checksum(encoded_message)
+    return headercreator.create_udp_header(self.port, self.port, len, summ)
+
+def connect():
+    #Setup the game tread
+    Snakegame = game.Snakegame()
+    gamethread = threading.Thread(target=Snakegame.startgame, args=(client_socket, client_address))
+    gamethread.start()
+
+    #opens Port and connects new players
+    ip = socket.gethostname()
+    port = 42069
+    sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
+    sock.bind((ip, port))
+    sock.settimeout(5)
+    sock.listen(2)
+    counter = 0
+    while True:
+        client_socket, client_address = sock.accept()
+        # Start a new thread to handle the player connection and give information to game thread
+        Snakegame.player_controller[counter] = client_socket
+        Snakegame.player_address[counter] = client_address
+        Snakegame.player_snake[counter] = game.Snake(0, 0, color[counter])
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address, Snakegame, counter))
+        client_thread.start()
+        counter = 1
+
+def handle_client(client_socket, client_address, game, snake):
+    while True:
+        try:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            header = headercreator.unpack_header(data[:8])
+            message = data[8:]
+            #Recieve new data until its the supposed length
+            while header[2] != len(message):
+                data = client_socket.recv(1024)
+                message += data
+            game.message[snake] = message
+        except socket.timeout:
+            send(client_socket, client_address, 'KeepAlive')
+    client_socket.close()
+    print("Connection closed for:", client_address)
+
+#splits byte string into parts of part_size
+def split_into_fixed_parts(data, part_size):
+    num_parts = (len(data) + part_size - 1) // part_size
+    return [data[i*part_size:(i+1)*part_size] for i in range(num_parts)]
